60839afddd6787bd10451ad3a13c1bee082a2791
Alvaro Miranda <kikitux@z2-3.kikitux.net>
2016-02-29 23:27:37 -0700

    tidy up

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -13,5 +13,8 @@ unset PACKAGES
 #
 #end
 
+sed -i '/tty/!s/mesg n/tty -s \&\& mesg n/' /home/vagrant/.profile
+sudo sed -i '/tty/!s/mesg n/tty -s \&\& mesg n/' /root/.profile
+
 sudo -E -H apt-get clean
 [ -f /etc/udev/rules.d/70-persistent-net.rule ] && sudo rm -f /etc/udev/rules.d/70-persistent-net.rule || true
